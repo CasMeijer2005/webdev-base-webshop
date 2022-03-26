47460dd19f3c07348bf68d9da79f5842b8f75581
CasMeijer2005 <90309791+CasMeijer2005@users.noreply.github.com>
2022-03-26 10:59:57 +0100

    grud admin user

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
-    include('core/header.php');
+include('core/db_connect.php');
 ?>
-Homepage
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop met een leuke naam</title>
+    <link rel="stylesheet" href="/webdev-base-webshop/assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="logo">hoodie</h1>
+        <nav>
+            <ul>
+                <li><a href="#"><span>boys</span></a></li>
+                <li><a href="#"><span>girls</span></a></li>
+                <li><a href="#"><span>other</span></a></li>
+            </ul>
+
+
+
+        </nav>
+    </header>
+</body>
+
+</html>
 <?php
-    include('core/footer.php');
+$con->close();
 ?>
